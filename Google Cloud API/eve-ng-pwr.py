87eddabc740a9e5ccce3ce5da51564e9362b82fb
Emeka Nwangwu <emekanwangwu96@gmail.com>
2020-05-11 00:24:03 -0500

    Added argsparse to enhance logic

---
+++ Google Cloud API/eve-ng-pwr.py
@@ -0,0 +1,32 @@
+from gcp_api_client import *
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Script to power on/off the Eve-NG lab hosted in GCP')
+    groups = parser.add_mutually_exclusive_group(required=False)
+
+    groups.add_argument('-start', action='store_true',
+    help="Starts the VM instance")
+    groups.add_argument('-stop', action='store_true',
+    help="Stops the VM instance")
+
+    args = parser.parse_args()
+    start_vm = args.start
+    stop_vm = args.stop
+
+    return start_vm, stop_vm
+
+def main():
+    get_args()
+    client = GCPClient('eve-ng-272521','us-central1-a','4587562327453364584')
+
+    if get_args()[0] == True: #This will start the VM
+        instance_control = client.start_instance()
+        pprint.pprint(instance_control)
+
+    else: #Otherwise we'll try to stop the VM
+        instance_control = client.stop_instance()
+        pprint.pprint(instance_control)
+
+if __name__ == '__main__':
+    main()
