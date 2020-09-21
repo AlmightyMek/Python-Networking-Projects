3d77f216195e950ce352e654bbd8673658499cf1
Emeka Nwangwu -X (enwangwu - EXPERIS US INC at Cisco) <enwangwu@cisco.com>
2020-09-21 17:31:17 -0500

    removed save_config func

--- EVE-API/eve_save_shut.py
+++ EVE-API/eve_save_shut.py
@@ -4,15 +4,13 @@ from dynamic_testbed import create_device_inventory
 from datetime import datetime
 import pprint
 
-def save_config():
-    startTime = datetime.now()
 
-    testbed = create_device_inventory() #from dynamic_testbed file
-    #pprint.pprint(testbed)
-    tb = load(testbed)
-    client = GenieClient(tb,log=False).save_device_config() #saves the devices configuration
+startTime = datetime.now()
 
-    total_time = (datetime.now() - startTime)
-    print('Script took {} to complete'.format(total_time))
+testbed = create_device_inventory() #from dynamic_testbed file
+#pprint.pprint(testbed)
+tb = load(testbed)
+client = GenieClient(tb,log=False).save_device_config() #saves the devices configuration
 
-save_config()
+total_time = (datetime.now() - startTime)
+print('Script took {} to complete'.format(total_time))
